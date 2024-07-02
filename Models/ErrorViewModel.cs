5da404d2a8588b95d99fa109bde93a0ef894705a
Caio Ferraz <caioferrazalmeida.27@gmail.com>
2024-07-02 13:00:51 -0300

    initial commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace calcmaster_webapp.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
